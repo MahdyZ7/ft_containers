06ba01ff6d8cbd7bc4dc3195e07ea6d9fea534e8
MahdyZ7 <amahdy9@gmail.com>
2023-03-04 19:43:13 +0400

    vector

--- ft_vector.hpp
+++ ft_vector.hpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <algorithm>
 #include <exception>
-#include <vector>
 #include <iterator>
 
 namespace ft
@@ -14,36 +13,60 @@ namespace ft
 	class vector
 	{
 	private:
-		
+		allocator_type::size_type	size;
 	public:
 		// constructors;
 		vector();
 		vector (const Allocator& alloc);
 		vector (typename std::allocator_traits<Allocator>::size_type n, const T& val,
-			const Allocator& alloc);
+					const Allocator& alloc);
 		template <class InputIterator> vector (InputIterator first,
-			InputIterator last, const Allocator& alloc);
+					InputIterator last, const Allocator& alloc);
 		vector (const vector& x);
+
+		// capacity
+		allocator_type::size_type size() const {return size;}
+		bool empty() const {return (size == 0);} //begin() == end()
+		allocator_type::size_type max_size() const {return (std::distance(begin(), end()));} // to be tested
+		// void reserve(allocator_type::size_type new_cap);
+		
+		// destructor
 		~vector();
 	};
 
 	template < class T, class Allocator>
 	ft::vector<T, Allocator>::vector(void)
 	{
+		begin = NULL;	
+	}
+
+	template < class T, class Allocator>
+	ft::vector<T, Allocator>::vector(const Allocator& alloc)
+	{
 		
 	}
 
 	template < class T, class Allocator>
-	vector<T, Allocator>::vector(void)
+	template <class InputIterator> ft::vector<T, Allocator>::vector(InputIterator first,
+			InputIterator last, const Allocator& alloc)
 	{
 		
 	}
+
+	template < class T, class Allocator>
+	ft::vector<T, Allocator>::vector(const vector& x)
+	{
+		
+	}
+
+	template < class T, class Allocator>
+	ft::vector<T, Allocator>::~vector()
+	{
+
+	}
+
 }
 
 	
-	// ft::vector::~vector()
-	// {
-
-	// }
 
 #endif
---
+++ main.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayassin <ayassin@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/04 12:08:16 by ayassin           #+#    #+#             */
+/*   Updated: 2023/03/04 15:54:01 by ayassin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include "ft_vector.hpp"
+#include <vector>
+#include <iostream>
+
+int main(void)
+{
+	std::vector<int> a = std::vector<int>();
+	std::cout << "Hello world";
+}
