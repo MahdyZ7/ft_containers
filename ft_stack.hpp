181e0622fbd4fe67ed7378a56d513c055683323a
MahdyZ7 <amahdy9@gmail.com>
2023-03-10 18:48:47 +0400

    stack baby steps

--- ft_stack.hpp
+++ ft_stack.hpp
@@ -6,7 +6,7 @@
 /*   By: ayassin <ayassin@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 10:07:05 by ayassin           #+#    #+#             */
-/*   Updated: 2023/03/09 11:09:40 by ayassin          ###   ########.fr       */
+/*   Updated: 2023/03/10 18:16:54 by ayassin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,13 @@
 
 namespace ft
 {
-	template <class T, class Container = deque<T> >
+	template <class T, class Container = std::deque<T> >
 	class stack{
+		protected:
+			Container c;
+		public:
+			stack() : c() {}
+			explicit stack(const Container& ctnr) : c(ctnr) {}
 		
 	};
 
