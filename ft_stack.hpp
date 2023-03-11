bf5dc62e05e46e3c9b927ca437f36cff063f0ea2
MahdyZ7 <amahdy9@gmail.com>
2023-03-11 18:28:12 +0400

    lala

--- ft_stack.hpp
+++ ft_stack.hpp
@@ -6,26 +6,32 @@
 /*   By: ayassin <ayassin@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 10:07:05 by ayassin           #+#    #+#             */
-/*   Updated: 2023/03/10 18:16:54 by ayassin          ###   ########.fr       */
+/*   Updated: 2023/03/11 17:48:13 by ayassin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_STACK_HPP
 # define FT_STACK_HPP
 
-#include <vector>
-#include <stack>
+#include "ft_vector.hpp"
+// #include <stack>
 
 namespace ft
 {
-	template <class T, class Container = std::deque<T> >
+	template <class T, class Container = ft::vector<T> >
 	class stack{
 		protected:
 			Container c;
 		public:
 			stack() : c() {}
 			explicit stack(const Container& ctnr) : c(ctnr) {}
-		
+			// explicit stack (const Container& ctnr = Container()) : c(ctnr) {}
+			bool empty() const {return c.empty();}
+			size_t size() const {return c.size();}
+			typename Container::value_type& top() {return c.back();}
+			void push (const typename Container::value_type& val) {c.push_back(val);}
+			void pop() {c.pop_back();}
+			void swap (stack& l){std::swap(c, l.c);} // to be replaced by ft
 	};
 
 	
--- main.cpp
+++ main.cpp
@@ -6,17 +6,54 @@
 /*   By: ayassin <ayassin@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 12:08:16 by ayassin           #+#    #+#             */
-/*   Updated: 2023/03/09 11:01:58 by ayassin          ###   ########.fr       */
+/*   Updated: 2023/03/11 14:37:36 by ayassin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_vector.hpp"
+// constructing stacks
+#include <iostream>       // std::cout
+#include <stack>          // std::stack
+#include <vector>         // std::vector
+#include <deque>          // std::deque
 #include "ft_stack.hpp"
-// #include <vector>
-#include <iostream>
 
-int main(void)
+int main ()
 {
-	// std::vector<int> a = std::vector<int>();
-	std::cout << "Hello world";
+	{
+		std::deque<int> mydeque (3,100);          // deque with 3 elements
+		std::vector<int> myvector (2,200);        // vector with 2 elements
+
+		std::stack<int> first;                    // empty stack
+		std::stack<int> second (mydeque);         // stack initialized to copy of deque
+
+		std::stack<int,std::vector<int> > third;  // empty stack using vector
+		std::stack<int,std::vector<int> > fourth (myvector);
+
+		std::cout << "size of first: " << first.size() << '\n';
+		std::cout << "size of second: " << second.size() << '\n';
+		std::cout << "size of third: " << third.size() << '\n';
+		std::cout << "size of fourth: " << fourth.size() << '\n';
+	}
+	{
+		std::deque<int> mydeque (3,100);          // deque with 3 elements
+		std::vector<int> myvector (2,200);        // vector with 2 elements
+
+		ft::stack<int> first;                    // empty stack
+		ft::stack<int> second (mydeque);         // stack initialized to copy of deque
+
+		ft::stack<int,std::vector<int> > third;  // empty stack using vector
+		ft::stack<int,std::vector<int> > fourth (myvector);
+
+		std::cout << "size of first: " << first.size() << '\n';
+		std::cout << "size of second: " << second.size() << '\n';
+		first.push(6);
+		first.push(12);
+		std::cout<< "The top element is " << first.top() << std::endl;
+		std::cout << "size of first: " << first.size() << '\n';
+		first.pop();
+		std::cout << "size of first: " << first.size() << '\n';
+		std::cout << "size of third: " << third.size() << '\n';
+		std::cout << "size of fourth: " << fourth.size() << '\n';
+	}
+  return 0;
 }
