4d30fdf2c5043a99cd9ad3f8b8a8e3a6a70ff5a4
MahdyZ7 <amahdy9@gmail.com>
2023-03-14 13:30:07 +0400

    mess

--- ft_vector.hpp
+++ ft_vector.hpp
@@ -2,6 +2,7 @@
 #define FT_VECTOR_HPP
 
 #include <iostream>
+#include <vector>
 // #include <algorithm>
 // #include <exception>
 // #include <iterator>
@@ -13,15 +14,33 @@ namespace ft
 	class vector
 	{
 	private:
-		size_t	p_size;
+		size_t		m_size;
+		size_t		capacity;
 		// allocator_traits<allocator_type>	begin;
 		// allocator_traits<allocator_type>	end;
-		T*									p_begin;
-		T*									p_end;
-		T*									p_end_of_storage;
-		Allocator 							m_alloc;
+		T*			data;
+		T*			p_begin;
+		T*			p_end;
+		T*			p_end_of_storage;
+		Allocator	myallocator;
 
 	public:
+
+		//type defs
+		typedef T											value_type;
+		typedef Allocator									allocator_type;
+		typedef typename allocator_type::reference			reference;
+		typedef typename allocator_type::const_reference	const_reference;
+		// typedef implementation-defined						iterator;
+		// typedef implementation-defined						const_iterator;
+		typedef typename allocator_type::size_type			size_type;
+		typedef typename allocator_type::difference_type	difference_type;
+		typedef typename allocator_type::pointer			pointer;
+		typedef typename allocator_type::const_pointer		const_pointer;
+		// typedef std::reverse_iterator<iterator>				reverse_iterator;
+		// typedef std::reverse_iterator<const_iterator>		const_reverse_iterator;
+
+
 		// constructors;
 		vector();
 		vector (const Allocator& alloc);
@@ -32,15 +51,39 @@ namespace ft
 		vector (const vector& x);
 
 		// capacity
-		size_t size() const {return p_size;}
+		size_t size() const {return m_size;}
 		bool empty() const {return (size() == 0);} //begin() == end()
-		size_t max_size() const {return (std::distance(p_begin(), p_end()));} // to be tested
+		size_t max_size() const {return (std::distance(p_begin(), p_end()));} // capacity to be tested
 		// void reserve(allocator_type::size_type new_cap);
 		
 		//modifiers
 		void	push_back(const T &val)
 		{
-			if (size == size)
+			if (capacity == 0)
+			{
+				//resurev(++capacity);
+			}
+			else if (m_size == capacity)
+			{
+				// reserve (capacity * 2)
+				// capacity <<= 2;
+				// realocate double the capacity
+			}
+			myallocator.construct(data + m_size, val);
+			// data[size++] = val;
+		}
+
+		void	resize(size_t n, T val = T())
+		{
+			while (m_size <  n)
+				(void) pop_back();
+			while (n < m_size)
+				push_back(val);
+
+		}
+
+		T	pop_back()
+		{
 			;
 		}
 		// destructor
@@ -53,6 +96,10 @@ namespace ft
 		p_begin = nullptr;
 		p_end = nullptr;
 		p_end_of_storage = nullptr;
+		capacity = 0;
+		m_size = 0;
+		
+		myallocator.allocate(0);
 		// begin = NULL;
 		// end = NULL;
 	}
@@ -60,26 +107,42 @@ namespace ft
 	template < class T, class Allocator>
 	ft::vector<T, Allocator>::vector(const Allocator& alloc)
 	{
+		p_begin = nullptr;
+		p_end = nullptr;
+		p_end_of_storage = nullptr;
+		capacity = 0;
+		m_size = 0;
 		
+		myallocator = alloc;
 	}
 
 	template < class T, class Allocator>
 	template <class InputIterator> ft::vector<T, Allocator>::vector(InputIterator first,
 			InputIterator last, const Allocator& alloc)
 	{
-		
+		myallocator.allocate(alloc.size());
+		for (typename T::iterator it = first; it != last; ++it)
+			push_back(*it);
 	}
 
 	template < class T, class Allocator>
 	ft::vector<T, Allocator>::vector(const vector& x)
 	{
+		if (this != &x)
+			return this;
+		for (typename T::iterator it = x.p_begin; it != p_end; ++it)
+			push_back(*it);
 		
 	}
 
 	template < class T, class Allocator>
 	ft::vector<T, Allocator>::~vector()
 	{
-
+		for (int i = 0; i < m_size; ++i)
+		{
+			myallocator.destroy(data + i);
+		}
+		myallocator.deallocate(data, capacity);
 	}
 
 }
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: ayassin <ayassin@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 12:08:16 by ayassin           #+#    #+#             */
-/*   Updated: 2023/03/13 15:34:17 by ayassin          ###   ########.fr       */
+/*   Updated: 2023/03/14 10:14:07 by ayassin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,45 +15,60 @@
 #include <stack>          // std::stack
 #include <vector>         // std::vector
 #include <deque>          // std::deque
-#include "ft_stack.hpp"
+#include "ft_vector.hpp"
+// #include "ft_stack.hpp"
 
-int main ()
+
+// stack main
+// int main ()
+// {
+// 	{
+// 		std::deque<int> myVector (3,100);          // deque with 3 elements
+// 		std::vector<int> myvector (2,200);        // vector with 2 elements
+
+// 		std::stack<int> first;                    // empty stack
+// 		std::stack<int> second (myVector);         // stack initialized to copy of deque
+
+// 		std::stack<int,std::vector<int> > third;  // empty stack using vector
+// 		std::stack<int,std::vector<int> > fourth (myvector);
+
+// 		std::cout << "size of first: " << first.size() << '\n';
+// 		std::cout << "size of second: " << second.size() << '\n';
+// 		std::cout << "size of third: " << third.size() << '\n';
+// 		std::cout << "size of fourth: " << fourth.size() << '\n';
+// 	}
+// 	{
+// 		// std::vector<int> myVector (3,100);          // deque with 3 elements
+// 		std::vector<int> myvector (2,200);        // vector with 2 elements
+
+// 		ft::stack<int> first;                    // empty stack
+// 		// ft::stack<int> second (myVector);         // stack initialized to copy of deque
+
+// 		ft::stack<int,std::vector<int> > third;  // empty stack using vector
+// 		ft::stack<int,std::vector<int> > fourth (myvector);
+
+// 		std::cout << "size of first: " << first.size() << '\n';
+// 		// std::cout << "size of second: " << second.size() << '\n';
+// 		first.push(6);
+// 		first.push(12);
+// 		std::cout<< "The top element is " << first.top() << std::endl;
+// 		std::cout << "size of first: " << first.size() << '\n';
+// 		first.pop();
+// 		std::cout << "size of first: " << first.size() << '\n';
+// 		std::cout << "size of third: " << third.size() << '\n';
+// 		std::cout << "size of fourth: " << fourth.size() << '\n';
+// 	}
+//   return 0;
+// }
+
+// vector main
+int	main()
 {
+	std::vector<int> a;
+	std::cout << a.capacity() << std::endl;
+	for (int i = 0; i < 100; ++i)
 	{
-		std::deque<int> myVector (3,100);          // deque with 3 elements
-		std::vector<int> myvector (2,200);        // vector with 2 elements
-
-		std::stack<int> first;                    // empty stack
-		std::stack<int> second (myVector);         // stack initialized to copy of deque
-
-		std::stack<int,std::vector<int> > third;  // empty stack using vector
-		std::stack<int,std::vector<int> > fourth (myvector);
-
-		std::cout << "size of first: " << first.size() << '\n';
-		std::cout << "size of second: " << second.size() << '\n';
-		std::cout << "size of third: " << third.size() << '\n';
-		std::cout << "size of fourth: " << fourth.size() << '\n';
+		a.push_back(i);
+		std::cout << a.capacity() << std::endl;
 	}
-	{
-		// std::vector<int> myVector (3,100);          // deque with 3 elements
-		std::vector<int> myvector (2,200);        // vector with 2 elements
-
-		ft::stack<int> first;                    // empty stack
-		// ft::stack<int> second (myVector);         // stack initialized to copy of deque
-
-		ft::stack<int,std::vector<int> > third;  // empty stack using vector
-		ft::stack<int,std::vector<int> > fourth (myvector);
-
-		std::cout << "size of first: " << first.size() << '\n';
-		// std::cout << "size of second: " << second.size() << '\n';
-		first.push(6);
-		first.push(12);
-		std::cout<< "The top element is " << first.top() << std::endl;
-		std::cout << "size of first: " << first.size() << '\n';
-		first.pop();
-		std::cout << "size of first: " << first.size() << '\n';
-		std::cout << "size of third: " << third.size() << '\n';
-		std::cout << "size of fourth: " << fourth.size() << '\n';
-	}
-  return 0;
 }
